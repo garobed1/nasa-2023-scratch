d57c7e138f81f2c27b99666c3399a146dfaf207a
Bedonian <gbedonia@nasmac3421.ndc.nasa.gov>
2023-06-20 17:30:41 -0700

    quest2klfield.py written, next is the augmentation to mk_presb.csh that dr nemec started

---
+++ quest2klfield.py
@@ -0,0 +1,122 @@
+import numpy as np
+import os, sys
+import argparse
+from rand_field import preprocess_data, get_kl_coefficients, truncated_karhunen_loeve_expansion
+"""
+Quest will output realizations of the KL coefficients to a database 
+file. Put those realizations through the rand_field.py code to produce
+characteristic paths. This script converts database values to a form 
+readable as arguments to rand_field.py. The actual path itself depends
+on the originating data, which is completely separate from Quest.
+"""
+
+"""
+Follow this convention: 
+# Var_Name Var_Value
+TEMP_path_0 T0
+TEMP_path_1 T1
+HUMIDITY_path_0 H0
+HUMIDITY_path_1 H1
+"""
+
+# start with one property for now
+
+# before anything, first estimate the covariance of the data
+# and find its eigenpairs
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true') 
+parser.add_argument('-n', '--numpoints', default=33) 
+args = parser.parse_args()
+verbose = args.verbose
+N = args.numpoints
+proplist = ['TEMP', 'HUMIDITY', 'PRESSURE', 'WINDX', 'WINDY']
+
+root = os.getcwd()
+quest_file = 'QUEST.dat'
+data_file = 'fulldata.json'
+case_dir = f'{root}/cases'
+
+if verbose:
+    print(f"Root dir: {root}")
+    # print(f" o Working in {db}")
+
+# first, generate the KL expansion
+if verbose:
+    print(f"Preprocessing data, {N} points per property")
+
+# begin loop over properties
+for prop in proplist:
+    trunc = None # different properties may call for different numbers of vars
+
+    if verbose:
+        print(f"Processing {prop}")
+    
+    altitudes, datat, means, stdvs, name = preprocess_data(data_file, N, prop)
+    Ndat = datat.shape[1]
+
+    # get kl coefficients
+    eigval, eigvec = get_kl_coefficients(datat, norm=False)
+
+    if verbose:
+        print(f"Generating {prop} profiles for all cases")
+
+    casestrlist = []
+    casenumlist = []
+    casecounter = 0
+    for case in os.listdir(case_dir):
+        if os.path.isdir(case_dir + '/' + case):
+            if verbose:
+                print(f"Generating {prop} profile for {case}")
+            casenum = int(case.split('.')[-1])
+            cur_file = case_dir + '/' + case + '/' + quest_file
+
+            datag = None
+            with open(cur_file) as cf:
+                # useful flags
+                lc = 0
+                newvar = False
+                propname = None
+                # begin line loop to count number of variables
+                if trunc is None:
+                    for line in cf:
+                        if line.startswith(f'KL_{prop}'): # only touch relevant lines
+                            lc += 1
+                        else:
+                            pass
+                    trunc = lc
+
+                if trunc == 0:
+                    pass
+
+                datag = np.zeros([trunc, 1])
+                for line in cf:
+                    if line.startswith(f'KL_{prop}'): # only touch relevant lines
+                        data = line.split(' ')
+                        ind = int(data[0].split('_')[-1])
+                        val = float(data[1])
+                        datag[ind] = val
+                    else:
+                        pass
+            
+            # now produce the path
+            pathgen = truncated_karhunen_loeve_expansion(datag, eigval, eigvec)
+            
+            # shift by means
+            for i in range(N):
+                # pathsgent[i,:] /= np.sqrt(stp[i])
+                pathgen[i,:] += means[i]
+            
+            # write to file
+            with open(case_dir + '/' + case + '/' + f'{prop}_profile.txt' , 'w') as wf:
+                for i in range(N):
+                    wf.write(f'{altitudes[i]} {pathgen[i][0]}\n')
+
+
+
+
+
+# find out how many variables were given in QUEST
+
+# naming convention
+# KL_{prop}_{num}
--- rand_field.py
+++ rand_field.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm, multivariate_normal
+from scipy.stats import norm, multivariate_normal, lognorm
 from scipy.sparse import diags
 from scipy.interpolate import interp1d
 import copy
@@ -21,12 +21,13 @@ at each point?
 Functions
 '''
 
-def unbiased_sample_cov_estimator(datat, norm = False):
+def unbiased_sample_cov_estimator(datat, norm = True):
     N, Ndat = datat.shape
 
-    fac = N-1
+    # was being silly here
+    fac = Ndat-1
     if norm:
-        fac = N
+        fac = Ndat
 
     mat = np.zeros([N,N])
     means = np.mean(datat, axis=1)
@@ -39,6 +40,7 @@ def unbiased_sample_cov_estimator(datat, norm = False):
 
 # produce 
 def truncated_karhunen_loeve_expansion(z, eigval, eigvec):
+
     trunc, Ngen = z.shape
     N = copy.deepcopy(eigval.shape[0])
 
@@ -57,30 +59,13 @@ def truncated_karhunen_loeve_expansion(z, eigval, eigvec):
 
     return W
 
-# import matplotlib
-# matplotlib.use('tkagg') 
-# generate distributed data from these distributions
 
-name = 'testprob'
-N = 33
-trunc = 15
-max_alt = 10000
-altitudes = np.linspace(0, max_alt, N)
-Ndat = 100
-Ndatplot = 100
-Ngen = 10
-means = np.log(np.linspace(1., 50., N))
-stdvs = np.linspace(3., 0.5, N)
-corrfrac = 1.
-# corr = max_alt/1. # correlation between adjacent points
 
-# options if we're pulling data
-prop = 'TEMP'
-if len(sys.argv) > 1:
-    dataname = sys.argv[1]
-    name = dataname.split('/')[-1]
+def preprocess_data(datapath, N, prop):
+    
+    name = datapath.split('/')[-1]
 
-    with open(dataname) as fj:
+    with open(datapath) as fj:
         fulldata = json.load(fj)
 
     datap_raw = np.array(fulldata[prop]).T
@@ -101,61 +86,136 @@ if len(sys.argv) > 1:
     means = np.mean(datap, axis=1)
     stdvs = np.std(datap, axis=1)
 
-    datat = copy.deepcopy(datap)
 
+    # already do this in covariance estimator
 
+    return altitudes, datap, means, stdvs, name
 
-else: # generate own data
-    cov = np.zeros([N,N])
-    for i in range(N):
-        for j in range(N):
-            cov[i,j] = np.exp(-abs(altitudes[i]-altitudes[j])/corr)
-
-    datap = multivariate_normal.rvs(mean=np.zeros(N), cov=cov, size=Ndat).T
-    datat = copy.deepcopy(datap)
-
-    # scale by varying data stdvs
-    for i in range(N):
-        datat[i,:] *= stdvs[i]
-        datat[i,:] += means[i]
         
-plt.plot(datat[:,:Ndatplot], altitudes)
-plt.xlabel(prop)
-plt.ylabel('Altitude (1000 ft)')
-plt.savefig(f'{name}_{prop}_pathsinit.png', bbox_inches="tight", dpi=500)
-plt.clf()
+
+def get_kl_coefficients(datat, norm=False):
+    # now we find the K-L expansion coefficients
+    # SPECIFICALLY FOR K(x1, x2) = e^-|x1 - x2|/corr, WE HAVE THE FOLLOWING EXACT
+    # QUEST MANUAL Sec 3.5.6
+
+    # ALTERNATIVELY, NUMERICALLY
+    meansest, covest = unbiased_sample_cov_estimator(datat, norm=norm)
+    eigval, eigvec = np.linalg.eig(covest)
+    return eigval, eigvec
 
 
-# now we find the K-L expansion coefficients
-# SPECIFICALLY FOR K(x1, x2) = e^-|x1 - x2|/corr, WE HAVE THE FOLLOWING EXACT
-# QUEST MANUAL Sec 3.5.6
+if __name__ == '__main__':
+    # needed parameters
+    name = 'testprob'
+    N = 33
+    trunc = 4
+    # options if we're pulling data
+    prop = 'HUMIDITY' #'TEMP'
+    #AND DATA AS COMMAND LINE ARGUMENT
 
-# ALTERNATIVELY, NUMERICALLY
-meansest, covest = unbiased_sample_cov_estimator(datat, norm=False)
-eigval, eigvec = np.linalg.eig(covest)
+    Ndatplot = 100
+    Ngen = 100
 
-# generate independent random gaussian vectors
-datag = np.zeros([trunc, Ngen])
-for i in range(trunc):
-    datag[i,:] = norm.rvs(size=Ngen)
+    # manufactured data
+    Ndat = 100
+    max_alt = 10000
+    corrfrac = 1.
+    altitudes = np.linspace(0, max_alt, N)
+    means = np.log(np.linspace(1., 50., N))
+    stdvs = np.linspace(3., 0.5, N)
+
+
+    # corr = max_alt/1. # correlation between adjacent points
+
+
+
+
+    # options for generating new paths
+    # will be determined by QUEST in practice
+    gstd = 1.0 # std deviation of the random KL coeffs
+    # or do list of length trunc
+    # gstd = [0.2, 0.2, 0.4, 0.5] # seems ok for temp, trunc 4?
+    # gstd = [0.8, 0.9, 0.1, 0.1]
+
+
+    # preprocess data
+    if len(sys.argv) > 1:
+        datapath = sys.argv[1]
+        altitudes, datat, means, stdvs, name = preprocess_data(datapath, N, prop)
+        Ndat = datat.shape[1]
+    else: # generate own data
+        cov = np.zeros([N,N])
+        for i in range(N):
+            for j in range(N):
+                cov[i,j] = np.exp(-abs(altitudes[i]-altitudes[j])/corr)
+
+
+        datap = multivariate_normal.rvs(mean=np.zeros(N), cov=cov, size=Ndat).T
+        datat = copy.deepcopy(datap)
+        # scale by varying data stdvs
+        for i in range(N):
+            datat[i,:] *= stdvs[i]
+            datat[i,:] += means[i]
+
+    # get kl coefficients
+    eigval, eigvec = get_kl_coefficients(datat, norm=False)
+
+    # generate independent random gaussian vectors for this example
+    datag = np.zeros([trunc, Ngen])
+    if not isinstance(gstd, list):
+        gstd = trunc*[gstd]
+    for i in range(trunc):
+        datag[i,:] = norm.rvs(size=Ngen, scale=gstd[i])
+
+    # generate new paths
+    pathsgen = truncated_karhunen_loeve_expansion(datag, eigval, eigvec)
+    pathsgent = copy.deepcopy(pathsgen)
+
+    stp = np.std(pathsgen, axis=1)
+    # shift by means
+    for i in range(N):
+        # pathsgent[i,:] /= np.sqrt(stp[i])
+        pathsgent[i,:] += means[i]
+
+    meanpaths = np.mean(pathsgent, axis=1)
+    stdvpaths = np.std(pathsgent, axis=1)
+
+    mpm1s = np.array([[means+stdvs], [means-stdvs]]).T.reshape([N, 2])
+    mpm1spaths = np.array([[meanpaths+stdvpaths], [meanpaths-stdvpaths]]).T.reshape([N, 2])
+    # plotting starts here
+    datplot = np.random.randint(0, Ndat, size = Ndatplot)
+    plt.plot(datat[:,datplot], altitudes, '-', linewidth=1.0)
+    plt.plot([], [], '-', linewidth=1.0,  label = 'Original Data')
+    plt.plot(means, altitudes, 'k-', linewidth=1.6)
+    plt.plot([], [], 'k-', linewidth=1.6, label = r'$\mu$')
+    plt.plot(mpm1s, altitudes, 'k--', linewidth=1.6)
+    plt.plot([], [], 'k--', linewidth=1.6, label = r'$\mu \pm 1\sigma$')
+    plt.legend()
+    plt.xlabel(prop)
+    plt.ylabel('Altitude (1000 ft)')
+    plt.savefig(f'{name}_{prop}_pathsinit.png', bbox_inches="tight", dpi=500)
+    plt.clf()
+
+    plt.plot(pathsgent, altitudes,  '-', linewidth=1.0)
+    plt.plot([], [], '-', linewidth=1.0,  label = 'Synthetic Data')
+    plt.plot(means, altitudes, 'k-', linewidth=1.6)
+    plt.plot([], [], 'k-', linewidth=1.6, label = r'$\mu$ (Original)')
+    plt.plot(mpm1s, altitudes, 'k--', linewidth=1.6)
+    plt.plot([], [], 'k--', linewidth=1.6, label = r'$\mu \pm 1\sigma$ (Original)')
+    plt.plot(meanpaths, altitudes, 'b-', linewidth=1.6)
+    plt.plot([], [], 'b-', linewidth=1.6, label = r'$\mu$ (Synthetic)')
+    plt.plot(mpm1spaths, altitudes, 'b--', linewidth=1.6)
+    plt.plot([], [], 'b--', linewidth=1.6, label = r'$\mu \pm 1\sigma$ (Synthetic)')
+    plt.xlabel(prop)
+    plt.ylabel('Altitude (1000 ft)')
+    plt.legend()
+    plt.savefig(f'{name}_{prop}_t{trunc}_pathsgen.png', bbox_inches="tight", dpi=500)
+    plt.clf()
+
+    
 
 # import pdb; pdb.set_trace()
 
-pathsgen = truncated_karhunen_loeve_expansion(datag, eigval, eigvec)
-pathsgent = copy.deepcopy(pathsgen)
-
-# shift by means
-for i in range(N):
-    pathsgent[i,:] /= stdvs[i]
-    pathsgent[i,:] += means[i]
-
-plt.plot(pathsgent, altitudes)
-plt.xlabel(prop)
-plt.ylabel('Altitude (1000 ft)')
-plt.savefig(f'{name}_{prop}_t{trunc}_pathsgen.png', bbox_inches="tight", dpi=500)
-plt.clf()
-
-import pdb; pdb.set_trace()
-
-
-
+# 1. preprocess_data
+# 2. get_kl_coefficients
+# 3. truncated_karhunen_loeve_expansion
