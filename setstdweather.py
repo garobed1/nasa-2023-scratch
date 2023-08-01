69ec8644018021368682c20ec73d006912ddb891
Bedonian <gbedonia@nasmac3421.ndc.nasa.gov>
2023-08-01 16:42:27 -0700

    added standard weather as option

---
+++ setstdweather.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os, sys
+import argparse
+from rand_field import preprocess_data
+from humidity_transform import hum2dew, dew2hum
+
+"""
+If there is no weather uncertainty being considered, use the 
+standard atmosphere (default sboom temp, 0 humidity)
+"""
+
+"""
+Follow this convention: 
+# Var_Name Var_Value
+TEMP_path_0 T0
+TEMP_path_1 T1
+
+HUMIDITY_path_0 H0
+HUMIDITY_path_1 H1
+"""
+
+# before anything, first estimate the covariance of the data
+# and find its eigenpairs
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true') 
+parser.add_argument('-d', '--datadir', action='store')
+# if given, don't convert humidity to dew point temperature, create KL, then convert back 
+# not recommended, better to convert to dew point using corresponding temp
+parser.add_argument('-hd', '--donthumiditytodew', action='store_true')
+
+# exclude a number of data points at the upper atmosphere 
+# last 4 points constitute data above ~61000 feet
+parser.add_argument('-e', '--exclude_upper', action='store', type=int, default=0)
+
+parser.add_argument('-n', '--numpoints', default=0) # if 0, use mean of altitudes as grid
+
+args = parser.parse_args()
+verbose = args.verbose
+Ngrid = args.numpoints
+datadir = args.datadir
+exclude = args.exclude_upper
+hd = args.donthumiditytodew
+# mhflag = args.modelhumidity
+
+proplist = ['HUMIDITY']
+
+root = os.getcwd()
+quest_file = 'QUEST.dat'
+
+data_file = 'fulldata.json'
+if datadir is not None:
+    data_file = datadir
+case_dir = f'{root}/cases'
+Ngen = len(os.listdir(case_dir))
+
+
+if verbose:
+    print(f"Root dir: {root}")
+    # print(f" o Working in {db}")
+
+# first, generate the KL expansion
+if verbose:
+    print(f"Preprocessing data (STD ATM ONLY) ...")
+
+# begin loop over properties
+for prop in proplist:
+
+    altitudes, datat, means, stdvs, name = preprocess_data(data_file, prop, Ngrid)
+
+
+    case_list = os.listdir(case_dir)
+    case_list = [x for x in case_list if not x.startswith('CFD_')]
+    for case in case_list:
+        if os.path.isdir(case_dir + '/' + case):
+            with open(case_dir + '/' + case + '/' + f'{prop}_profile.txt' , 'w') as wf:
+                for i in range(N):
+                    wf.write(f'{altitudes[i]} {0.0:1.13f}\n')
+
